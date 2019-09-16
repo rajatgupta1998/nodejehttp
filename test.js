d48ec36b9bdf4efefce340c4722caa0a2557375e
Rajat Gupta <rajatgupta1998@acm.org>
2019-09-16 21:49:24 +0530

    Don't write unnecessary responses for now, only JSON
    Signed-off-by: Rajat Gupta <rajatgupta1998@acm.org>

--- test.js
+++ test.js
@@ -1,17 +1,13 @@
 const http = require('http')
 
 var server = http.createServer((req,res) => {
-    console.log(req.url)
-    // res.write("Hi, you are on " + req.url)
 
    res.writeHead(200,{"Content-Type" : "application/json"})
 
     if(req.url == '/users'){
-        res.write('on /users')
         res.end("{'name': 'test ','abc':'pqr'}");
     }
     if(req.url == '/logout'){
-        res.write('on /logout')
         res.end("{'id': '1711981227', 'sname' : 'Gupta'}");
     }
 
